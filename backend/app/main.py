0957e9fe5966cdd73f6b62d18a3da59f27a6423a
Pranav <pranav.modi@gmail.com>
2024-06-24 11:59:26 +0530

    update code

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,7 +4,8 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
-@app.route('/api/hello', methods=['GET'])
+
+@app.route('/', methods=['GET'])
 def hello():
     return jsonify(message='Hello from Flask!')
 
