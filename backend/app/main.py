69a674ba8820ca9d85571d3bdc0edb6eb615e680
Pranav <pranav.modi@gmail.com>
2024-06-24 11:22:23 +0530

    update code

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
-@app.route('/')
-def hello_world():
-    return 'Hello, fucking World!'
+@app.route('/api/hello', methods=['GET'])
+def hello():
+    return jsonify(message='Hello from Flask!')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
